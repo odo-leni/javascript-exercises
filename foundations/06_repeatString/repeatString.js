9e4f7f1d8b7ac24247335692f5174dd64864bb64
Olivier DONEAU <odoneau@leni.fr>
2026-01-12 15:41:13 +0100

    fix: solve "06_repeatString" exercise in "foundations"

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, repeat) {
+  if (repeat >= 0) {
+    let result = "";
 
+    for (i = 0; i < repeat; i++) {
+      result += string;
+    }
+
+    return result;
+  }
+  else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
