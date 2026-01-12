38b34e3e3c280a5566e098655baa5707d1e8145b
Olivier DONEAU <odoneau@leni.fr>
2026-01-12 17:01:30 +0100

    fix: solve "11_tempConversion" exercise in "foundations"

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let result = 0;
+
+  result = (temp - 32) * (5 / 9);
+
+  if (!Number.isInteger(result)) {
+    result = result.toFixed(1);
+    result = parseFloat(result);
+  }
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let result = 0;
+
+  result = (temp * (9 / 5)) + 32;
+  
+  if (!Number.isInteger(result)) {
+    result = result.toFixed(1);
+    result = parseFloat(result);
+  }
+
+  return result;
 };
 
 // Do not edit below this line
