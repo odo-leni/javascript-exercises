2e202bc1d076c0b9e6430afd8f39faf039bdb1d1
Olivier DONEAU <odoneau@leni.fr>
2026-01-12 16:19:40 +0100

    fix: solve "09_sumAll" exercise in "foundations"

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (firstValue, secondValue) {
+  if (firstValue < 0 || secondValue < 0) {
+    return "ERROR";
+  }
 
+  if (!Number.isInteger(firstValue) || !Number.isInteger(secondValue)) {
+    return "ERROR";
+  }
+
+  let start = firstValue;
+  let end = secondValue;
+
+  let result = 0;
+
+  if (secondValue < firstValue) {
+    start = secondValue;
+    end = firstValue;
+  }
+
+  for (i = start; i <= end; i++) {
+    result += i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
