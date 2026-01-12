9a8c810dbcded4ec0aaffe02defba68fb72b7d5b
Olivier DONEAU <odoneau@leni.fr>
2026-01-12 16:33:54 +0100

    fix: solve "10_leapYears" exercise in "foundations"

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let result = false;
 
+  result = year % 4 === 0;
+
+  if (result && (year % 100 === 0)) {
+    result = (year % 400 === 0);
+  }
+
+  return result;
 };
 
 // Do not edit below this line
