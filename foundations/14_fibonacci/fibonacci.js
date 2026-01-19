ac573038f21303ef691fdfcfeaa4bf21811c2ab0
Olivier DONEAU <odoneau@leni.fr>
2026-01-19 16:21:13 +0100

    fix: solve "14_fibonacci" exercise in "foundations"

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  const n = parseInt(number, 10);
 
+  if (n < 0) {
+    return "OOPS";
+  }
+
+  if (n === 0) {
+    return 0;
+  }
+
+  if (n === 1) {
+    return 1;
+  }
+
+  const fb = function (number) {
+    if (number === 0) {
+      return 0;
+    }
+    if (number === 1) {
+      return 1;
+    }
+    return fb(number - 1) + fb(number - 2);
+  }
+
+  return result = fb(n - 1) + fb(n - 2);
 };
 
 // Do not edit below this line
