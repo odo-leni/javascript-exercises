60a80ff73420e6feb99f51ef90619d3dc8e7a340
Olivier DONEAU <odoneau@leni.fr>
2026-01-19 15:35:12 +0100

    fix: solve "13_palindromes" exercise in "foundations"

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let cleaned = string.replace(/[^\w]/g, ''); // matches any character that is not a word character (letters, numbers, underscores)
+  cleaned = cleaned.replaceAll("_", ""); // remove underscores
+  const reverseCleaned = cleaned.split('').reverse().join('');
+  //console.log(cleaned.toLowerCase());
+  //console.log(reverseCleaned.toLowerCase());
+  return cleaned.toLowerCase() === reverseCleaned.toLowerCase();
 };
 
 // Do not edit below this line
