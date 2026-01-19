911fa19991039039b9656272197fc9d90ed7168d
Olivier DONEAU <odoneau@leni.fr>
2026-01-19 16:51:34 +0100

    fix: solve "16_findTheOldest" exercise in "foundations"

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  const result = array.map(p => {
+    if (p.yearOfDeath) {
+      p.age = p.yearOfDeath - p.yearOfBirth;
+    }
+    else {
+      p.age = new Date().getFullYear() - p.yearOfBirth;
+    }
+    return p;
+  });
+  return result.sort((a, b) => a.age - b.age).reverse()[0];
 };
 
 // Do not edit below this line
